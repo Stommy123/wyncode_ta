854b4adfa85ae10d329a6960c84a6b6e8666d71b
Stommy123 <Stommy49@Yahoo.com>
2018-10-01 16:59:49 -0400

    object syntax

---
+++ object/human.js
@@ -0,0 +1,19 @@
+let human = {
+  walk: function() {
+    console.log("I'm walking")
+  },
+  talk: function() {
+    console.log("I'm talking")
+  },
+  eat: function() {
+    console.log("I'm eating")
+  },
+  sleep: function() {
+    console.log("I'm sleeping")
+  }
+}
+
+human.walk()
+human.talk()
+human.eat()
+human.sleep()
