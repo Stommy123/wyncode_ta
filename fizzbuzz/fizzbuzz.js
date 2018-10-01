649432d7d96cea17eee6a841251c519d87028934
Stommy123 <Stommy49@Yahoo.com>
2018-10-01 14:00:28 -0400

    changed fizzbuzz

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -1,21 +1,24 @@
+// let array = Array.from(Array(100).keys())
+
+
 let array = [3,5,15,1]
 
 array.map(element => {
   if (element % 3 === 0 && element % 5 === 0) {
     element = "fizzbuzz"
-    console.log(element)
+    // console.log(element)
   }
   else if (element % 3 === 0) {
     element = "fizz"
-    console.log(element)
+    // console.log(element)
   }
   else if (element % 5 === 0) {
     element = "buzz"
-    console.log(element)
+    // console.log(element)
   }
   else {
     element
-    console.log(element)
+    // console.log(element)
   }
   return element
 })
