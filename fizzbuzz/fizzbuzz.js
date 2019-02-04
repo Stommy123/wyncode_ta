806c8aac0f65d426d305e8590ffc12a87f0d6195
Tommy <tsuwunrut@carecloud.com>
2019-02-04 10:39:23 -0500

    fizz

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -1,5 +1,5 @@
 for (let i=0; i <= 100; i++) {
-  if (i % 15 === 0) console.log("FizzBuzz")
+  if (i % 3 === 0 && i % 5 === 0) console.log("FizzBuzz")
   else if (i % 3 === 0) console.log("Fizz")
   else if (i % 5 === 0) console.log("Buzz")
   else console.log(i)
